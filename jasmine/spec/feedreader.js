592dca346946d637255d7b03f62d2bc1d2678d0c
Vinicius Silva <viniciush307@gmail.com>
2018-01-13 17:42:57 -0200

    adding unit test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -21,52 +21,101 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('Are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        it('Allfeeds should have a url defined', function () {
+          allFeeds.forEach(function (feed) {
+              expect(feed.url).toBeDefined();
+          })
+        });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        it('Allfeeds should have a name defined and not empty', function () {
+          allFeeds.forEach(function (feed) {
+            expect(feed.name).toBeDefined();
+            expect(feed.name.length).not.toBe(0);
+          })
+        });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function () {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        it('the menu element should be hide by default', function () {
+          var menu = $('body').attr("class");
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+          expect(menu).toBe('menu-hidden');
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+        });
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+       it('the menu element should be hide by default', function () {
+         var menu = $('body');
+         var menuIcon = $('.menu-icon-link')[0];
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+         menuIcon.click();
+         expect(menu.attr("class")).not.toBe('menu-hidden');
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+         menuIcon.click();
+         expect(menu.attr("class")).toBe('menu-hidden');
+
+       });
+
+
+    });
+
+    describe('Initial Entries', function () {
+
+      beforeEach(function(done) {
+        setTimeout(function() {
+          loadFeed(0, function () {
+            done();
+          });
+
+        }, 2000);
+      });
+
+
+      it('the loadFeed function should execution correctly', function (done) {
+        var feed = $('.feed .entry');
+
+        expect(feed.length).not.toBe(0);
+        done();
+      });
+
+    });
+
+    describe('New Feed Selection', function () {
+
+      beforeEach(function(done) {
+        setTimeout(function() {
+          loadFeed(0, function () {
+            done();
+          });
+        }, 2000);
+      });
+
+      it('Guarantee that when load a new content the feed is change', function (done) {
+        var feed = $('.feed .entry-link');
+
+        expect(feed.length).not.toBe(0);
+        expect(feed.attr("href")).toContain('http://blog.udacity.com');
+
+        setTimeout(function() {
+          loadFeed(2, function () {
+            var feed = $('.feed .entry-link');
+
+            expect(feed.length).not.toBe(0);
+            expect(feed.attr("href")).toContain('http://feedproxy.google.com');
+
+            done();
+          });
+        }, 2000);
+
+      });
+
+    });
 }());
